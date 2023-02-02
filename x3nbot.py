0bb34bc3a37e62ad16dd6bac8308b498161dfda3
kkqbutwebhooker <77482156+kkqbutwebhooker@users.noreply.github.com>
2023-02-02 19:34:43 +0300

    Add files via upload

---
+++ x3nbot.py
@@ -0,0 +1,57 @@
+import discord
+from discord.ext import commands
+import youtube_dl
+
+bot = commands.Bot(command_prefix='/')
+
+@bot.command()
+async def play(ctx, url: str):
+    song_there = os.path.isfile("song.mp3")
+    try:
+        if song_there:
+            os.remove("song.mp3")
+            print("Removed oldsong file")
+    except PermissionError:
+        print("Trying to delete song file, but it's being played")
+        await ctx.send("Error: Music playing")
+        return
+
+    await ctx.send("Getting everything ready now")
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        print("Downloading audio now\n")
+        ydl.download([url])
+
+    for file in os.listdir("./"):
+        if file.endswith(".mp3"):
+            name = file
+            print(f"Renamed File: {file}\n")
+            os.rename(file, "song.mp3")
+
+    voice = get(bot.voice_clients, guild=ctx.guild)
+
+    if voice and voice.is_connected():
+        await voice.disconnect()
+        print("Music stopped")
+
+    channel = ctx.message.author.voice.channel
+    voice = get(bot.voice_clients, guild=ctx.guild)
+
+    if voice and voice.is_connected():
+        await voice.move_to(channel)
+    else:
+        voice = await channel.connect()
+
+    await ctx.send(f"Playing: {name}")
+    voice.play(discord.FFmpegPCMAudio("song.mp3"), after=lambda e: print(f"{name} has finished playing"))
+    voice.source = discord.PCMVolumeTransformer(voice.source)
+    voice.source.volume = 0.07
